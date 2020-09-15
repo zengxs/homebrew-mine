85053814201560a31ec41682d08f07c859a5ad52
zengxs <xiangsong.zeng@outlook.com>
2020-09-15 20:15:56 +0800

    xcbeautify 0.8.1

--- Formula/xcbeautify.rb
+++ Formula/xcbeautify.rb
@@ -1,8 +1,8 @@
 class Xcbeautify < Formula
   desc "Little beautifier tool for xcodebuild"
   homepage "https://github.com/thii/xcbeautify"
-  url "https://github.com/thii/xcbeautify/releases/download/0.7.4/xcbeautify-0.7.4-x86_64-apple-macosx.zip"
-  sha256 "ea1d194624b9309c92a310375d0b6fa679ff9ad5a4780db4e83907bf459e973e"
+  url "https://github.com/thii/xcbeautify/releases/download/0.8.1/xcbeautify-0.8.1-x86_64-apple-macosx.zip"
+  sha256 "75e9d345a68a759ab268f0ecef4373a0ae590b07939096a076bdb25f20e69143"
 
   bottle :unneeded
 
