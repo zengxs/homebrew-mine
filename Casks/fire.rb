12665ab78bdee2e119b3f1333318f51b8b7c139a
zengxs <xiangsong.zeng@gmail.com>
2020-10-04 11:51:13 +0800

    Fire (new cask)

---
+++ Casks/fire.rb
@@ -0,0 +1,10 @@
+cask 'fire' do
+  version '10.0.0.2557'
+  sha256 '4bc90e7bc52793812a0c0496d1cbc2ba09b6aa5f37ed70db2305b937dd35478f'
+  url "https://downloads.elementscompiler.com/RemObjects%20Fire%20-%20#{version}.dmg"
+
+  name 'Fire'
+  homepage 'https://www.elementscompiler.com/elements/'
+
+  app 'Fire.app'
+end
