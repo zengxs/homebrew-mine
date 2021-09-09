2dec1a1c839ba556de2c46e0e46ae0e5c53e3363
zengxs <xiangsong.zeng@outlook.com>
2021-09-09 15:38:59 +0800

    rider-preview 2021.2.1 (new cask)

---
+++ Casks/rider-preview.rb
@@ -0,0 +1,49 @@
+cask "rider-preview" do
+  version "2021.2.1,212.5080.71"
+
+  if Hardware::CPU.intel?
+    sha256 "6d3d71c5a7ea0443427903e6749e19d808dbb9280f51d96d61584d6f2e105500"
+    url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.before_comma}.dmg"
+  else
+    sha256 "30957466ba82b956e6a89433cca69dcc9118a202e67979938545dbbfd87b670d"
+    url "https://download.jetbrains.com/rider/JetBrains.Rider-#{version.before_comma}-aarch64.dmg"
+  end
+
+  name "JetBrains Rider"
+  desc ".NET IDE"
+  homepage "https://www.jetbrains.com/rider/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=RD&latest=true&type=release"
+    strategy :page_match do |page|
+      JSON.parse(page)["RD"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
+
+  conflicts_with cask: "rider"
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "Rider.app"
+
+  uninstall_postflight do
+    ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "rider") }.each do |path|
+      if File.exist?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+        File.delete(path)
+      end
+    end
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Rider#{version.major_minor}",
+    "~/Library/Caches/Rider#{version.major_minor}",
+    "~/Library/Logs/Rider#{version.major_minor}",
+    "~/Library/Preferences/Rider#{version.major_minor}",
+    "~/Library/Preferences/jetbrains.rider.71e559ef.plist",
+    "~/Library/Saved Application State/com.jetbrains.rider.savedState",
+  ]
+end
